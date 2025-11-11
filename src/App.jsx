3ceef54675f26ad87b83d3413a03b4da20cb93bc
jiyoung <jenn4843@naver.com>
2025-11-12 00:58:20 +0900

    [FE]FIX: 카카오 콜백 추가

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,19 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Login from "./pages/Login/Login.jsx";
 import Signup from "./pages/SignUp/SignUp.jsx";
+import KakaoCallback from "./pages/Login/KakaoCallback.jsx";
 import LetterRoom from "./pages/LetterRoom/LetterRoom.jsx";
 import LetterRoomCreate from "./pages/LetterRoomCreate/LetterRoomCreate.jsx";
 import LetterRoomOpen from "./pages/LetterRoom/LetterRoomOpen.jsx";
 import LetterRoomLocked from "./pages/LetterRoom/LetterRoomLocked";
 
-
 export default function App() {
   return (
     <Routes>
       <Route path="/" element={<Login />} />
+      <Route path="/login" element={<Login />} />
       <Route path="/signup" element={<Signup />} />
+      <Route path="/auth/kakao/callback" element={<KakaoCallback />} />
       <Route path="/letterroom" element={<LetterRoom />} />
       <Route path="/letterroom/create" element={<LetterRoomCreate />} />
       <Route path="/letterroom/open/:id" element={<LetterRoomOpen />} />
